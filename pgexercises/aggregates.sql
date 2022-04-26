741a61cbf1d6c74e0fd9e63b03a97db2a8fd2e1a
Jason Wang <jasonherngwang@gmail.com>
2022-04-26 22:46:53 +0000

    Completed more aggregates problems.

--- pgexercises/aggregates.sql
+++ pgexercises/aggregates.sql
@@ -54,10 +54,10 @@ SELECT count(DISTINCT memid)
 -- Question 8. List facilities with more than 1000 slots booked
 SELECT facid, "Total Slots"
   FROM (SELECT facid,
-		       sum(slots) AS "Total Slots"
-		  FROM cd.bookings
-		 GROUP BY facid
-	   ) AS total_slots
+               sum(slots) AS "Total Slots"
+          FROM cd.bookings
+         GROUP BY facid
+       ) AS total_slots
  WHERE "Total Slots" > 1000
  ORDER BY facid
 
@@ -71,9 +71,9 @@ HAVING sum(slots) > 1000
 -- Question 9. Find the total revenue of each facility
 SELECT f.name,
        sum(CASE WHEN b.memid = 0
-	            THEN f.guestcost * b.slots
-		 	        ELSE f.membercost * b.slots
-	         END) AS revenue
+              THEN f.guestcost * b.slots
+              ELSE f.membercost * b.slots
+           END) AS revenue
   FROM cd.facilities f
   JOIN cd.bookings b
     ON b.facid = f.facid
@@ -81,9 +81,126 @@ SELECT f.name,
  ORDER BY revenue;
 
 -- Question 10. Find facilities with a total revenue less than 1000
+SELECT name,
+       revenue
+  FROM (SELECT name,
+               sum(CASE WHEN b.memid = 0
+                        THEN f.guestcost * b.slots
+                        ELSE f.membercost * b.slots
+                    END
+                  ) AS revenue
+          FROM cd.facilities f
+          JOIN cd.bookings b
+            ON f.facid = b.facid
+         GROUP BY f.facid
+       ) AS total_revenue
+ WHERE revenue < 1000
+ ORDER BY revenue;
+
 -- Question 11. Output the facility id that has the highest number of slots booked
+SELECT facid,
+       total_slots AS "Total Slots"
+  FROM (SELECT facid,
+               sum(slots) AS total_slots
+          FROM cd.bookings
+         GROUP BY facid
+       ) AS total_slots_by_facility
+ WHERE total_slots = (SELECT max(total_slots)
+			          		    FROM (SELECT facid,
+                                     sum(slots) AS total_slots
+                                FROM cd.bookings
+                               GROUP BY facid
+                             ) AS total_slots_by_facility
+                     );
+
+-- Using a CTE can reduced repeated queries
+WITH sum AS (SELECT facid,
+                    sum(slots) AS total_slots
+               FROM cd.bookings
+              GROUP BY facid
+            )
+SELECT facid,
+       total_slots AS "Total Slots"
+  FROM sum
+ WHERE total_slots = (SELECT max(total_slots)
+                        FROM sum
+                     );
+       
+
 -- Question 12. List the total slots booked per facility per month, part 2
+SELECT facid,
+       date_part('month', starttime) AS month,
+       sum(slots) as slots
+  FROM cd.bookings
+ WHERE date_part('year', starttime) = 2012
+ GROUP BY facid, month
+ 
+ UNION ALL
+
+SELECT facid,
+       NULL,
+       sum(slots) as slots
+  FROM cd.bookings
+ WHERE date_part('year', starttime) = 2012
+ GROUP BY facid
+
+ UNION ALL
+
+ SELECT NULL,
+        NULL,
+        sum(slots) as slots
+  FROM cd.bookings
+  WHERE date_part('year', starttime) = 2012
+ORDER BY facid, month
+
+-- Using CTE
+WITH bookings AS (SELECT facid,
+                         date_part('month', starttime) AS month,
+                         sum(slots) as slots
+                    FROM cd.bookings
+                   WHERE date_part('year', starttime) = 2012
+                   GROUP BY facid, month
+                 )
+SELECT *
+  FROM bookings
+
+ UNION ALL
+ 
+SELECT facid,
+       NULL,
+       sum(slots) as slots
+  FROM bookings       
+       GROUP BY facid
+       
+ UNION ALL
+ 
+SELECT NULL,
+       NULL,
+       sum(slots) as slots
+  FROM bookings
+       
+ ORDER BY facid, month
+ 
+-- Using rollup
+SELECT facid,
+       extract('month' FROM starttime) AS month,
+       sum(slots) as slots
+  FROM cd.bookings
+ WHERE extract('year' FROM starttime) = 2012
+ GROUP BY rollup(facid, month)
+ ORDER BY facid, month;
+
+
 -- Question 13. List the total hours booked per named facility
+SELECT f.facid,
+       f.name,
+	     trim(to_char(sum(b.slots) / 2.0, '99999999D99')) AS "Total Hours"
+  FROM cd.bookings b
+  JOIN cd.facilities f
+    ON b.facid = f.facid
+ GROUP BY f.facid, f.name
+ ORDER BY f.facid;
+
 -- Question 14. List each member's first booking after September 1st 2012
 -- Question 15. Produce a list of member names, with each row containing the total member count
 -- Question 16. Produce a numbered list of members
